19dd0f52f7d12a3bc413c3e2caa36e557f2263f0
serak0310 <serak0310@naver.com>
2024-08-26 10:14:11 +0900

    :sparkles: Feat: 장바구니 조회 API 생성

---
+++ src/main/java/com/ite/sws/domain/cart/vo/CartItemVO.java
@@ -0,0 +1,30 @@
+package com.ite.sws.domain.cart.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * CartItem VO
+ * @author 김민정
+ * @since 2024.08.26
+ * @version 1.0
+ *
+ * <pre>
+ * 수정일        수정자       수정내용
+ * ----------  --------    ---------------------------
+ * 2024.08.26  	김민정       최초 생성
+ * </pre>
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class CartItemVO {
+    private Long productId;
+    private int quantity;
+    private String productName;
+    private int productPrice;
+    private String productThumbnail;
+}
