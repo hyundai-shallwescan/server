f44e0abdaedf4902fba55aa9926a298127adf920
serak0310 <serak0310@naver.com>
2024-09-08 18:51:18 +0900

    :sparkles: Feat: 장바구니 변경 시, 관련 채팅 발송

--- src/main/java/com/ite/sws/domain/cart/dto/CartItemChatDTO.java
+++ src/main/java/com/ite/sws/domain/cart/dto/CartItemChatDTO.java
@@ -7,28 +7,27 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * 장바구니 아이템 상세 DTO
+ * 장바구니 아이템 채팅 DTO
  * @author 김민정
- * @since 2024.09.05
+ * @since 2024.09.08
  * @version 1.0
  *
  * <pre>
  * 수정일        수정자       수정내용
  * ----------  --------    ---------------------------
- * 2024.09.05  	김민정       최초 생성
+ * 2024.09.08  	김민정       최초 생성
  * </pre>
  */
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Getter
-public class CartItemMessageDTO {
-    private Long cartId;
-    private Long productId;
+public class CartItemChatDTO {
+    private String cartMemberName;
+    @Setter
+    private String action; // "create", "increase", "decrease", "delete"
     private Integer quantity;
     private String productName;
     private Integer productPrice;
     private String productThumbnail;
-    @Setter
-    private String action; // "create", "increase", "decrease", "delete"
 }
