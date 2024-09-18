54ddd12d88970f3a7c6134ed58ec0b6c388a7e38
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-18 14:32:52 +0900

    :pencil2: Chore: nginx 빌드 경로 수정

--- script/after-deploy.sh
+++ script/after-deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 REPOSITORY=/home/api/
 
-cd $REPOSITORY/api_back
+cd $REPOSITORY/api_back || { echo "Failed to navigate to $REPOSITORY/api_back"; exit 1; }
 
-echo "> 🔵 Stop & Remove docker services."
-cd ..
-docker compose down
+echo "> 🔵 Stop & Remove Docker services."
+docker compose down || { echo "Failed to stop Docker services"; exit 1; }
 
-echo "> 🟢 Run new docker services."
-docker compose up --build -d
+echo "> 🟢 Run new Docker services."
+docker compose up --build -d || { echo "Failed to start Docker services"; exit 1; }
