487fe541d52fa686c8a3df70ddc4b9655a05ada7
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-19 09:39:31 +0900

    :pencil2: Chore: 서버 도커 컨테이너에 띄우도록 수정:

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,26 +1,35 @@
-nano /home/ec2-user/action/deploy.sh
 #!/bin/bash
 
 BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
+IMAGE_NAME="shall-we-image"
+CONTAINER_NAME="shall-we-server-container"
+
 echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
 
-echo ">>> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/action/
-cp $BUILD_JAR $DEPLOY_PATH
-
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
-else
-  echo ">>> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+if [ -z "$BUILD_JAR" ]; then
+  echo ">>> No JAR file found. Exiting." >> /home/ec2-user/action/deploy.log
+  exit 1
 fi
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+echo ">>> build 파일 복사" >> /home/ec2-user/action/deploy.log
+
+cat <<EOF > /home/ec2-user/action/Dockerfile
+FROM openjdk:8-jdk-alpine
+COPY $BUILD_JAR /app/$JAR_NAME
+WORKDIR /app
+CMD ["java", "-jar", "$JAR_NAME"]
+EOF
+
+docker build -t $IMAGE_NAME /home/ec2-user/action
+
+if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
+  echo ">>> Stopping and removing existing container: $CONTAINER_NAME" >> /home/ec2-user/action/deploy.log
+  docker stop $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+fi
+
+echo ">>> Starting new container: $CONTAINER_NAME" >> /home/ec2-user/action/deploy.log
+docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
+
+echo ">>> Application deployed in Docker container: $CONTAINER_NAME" >> /home/ec2-user/action/deploy.log
