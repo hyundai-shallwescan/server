a0ffda022183042e891dcc6489d1575a1e9b0be0
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-22 22:14:19 +0900

    :pencil2: Chore: Build 경로 변경

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-BUILD_JAR=$(ls /home/ec2-user/action/build/libs/*.jar)
+# Define variables
+BUILD_JAR=$(ls /home/ec2-user/action/target/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 IMAGE_NAME="shall-we-image"
 CONTAINER_NAME="shall-we-server-container"
+LOG_FILE="/home/ec2-user/action/deploy.log"
 
-echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/action/deploy.log
+echo ">>> Build 파일명: $JAR_NAME" >> $LOG_FILE
 
 if [ -z "$BUILD_JAR" ]; then
-  echo ">>> No JAR file found. Exiting." >> /home/ec2-user/action/deploy.log
+  echo ">>> No JAR file found. Exiting." >> $LOG_FILE
   exit 1
 fi
 
-echo ">>> build 파일 복사" >> /home/ec2-user/action/deploy.log
+echo ">>> Build 파일 복사" >> $LOG_FILE
 
 cat <<EOF > /home/ec2-user/action/Dockerfile
-FROM openjdk:8-jdk-alpine
-COPY $BUILD_JAR /app/$JAR_NAME
-WORKDIR /app
-CMD ["java", "-jar", "$JAR_NAME"]
+FROM tomcat:8-jdk8-openjdk
+COPY $BUILD_JAR /opt/tomcat/webapps/$JAR_NAME
+CMD ["catalina.sh", "run"]
 EOF
 
 docker build -t $IMAGE_NAME /home/ec2-user/action
 
 if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
-  echo ">>> Stopping and removing existing container: $CONTAINER_NAME" >> /home/ec2-user/action/deploy.log
+  echo ">>> Stopping and removing existing container: $CONTAINER_NAME" >> $LOG_FILE
   docker stop $CONTAINER_NAME
   docker rm $CONTAINER_NAME
 fi
 
-echo ">>> Starting new container: $CONTAINER_NAME" >> /home/ec2-user/action/deploy.log
+echo ">>> Starting new container: $CONTAINER_NAME" >> $LOG_FILE
 docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
 
-echo ">>> Application deployed in Docker container: $CONTAINER_NAME" >> /home/ec2-user/action/deploy.log
+echo ">>> Application deployed in Docker container: $CONTAINER_NAME" >> $LOG_FILE
